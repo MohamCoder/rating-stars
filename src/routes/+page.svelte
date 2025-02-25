d5db8a86d4233a0a8be18e8ab90ad72905e9690a
MohamCoder <mohammedmus1000@gmail.com>
2025-02-26 00:16:22 +0300

    home page finished

---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+  import BorderedButton from "$lib/component/BorderedButton.svelte";
+  import Button from "$lib/component/Button.svelte";
+</script>
+
+<div class="main-star"></div>
+
+<navbar class="flex items-center justify-between mx-64 mt-8 h-10">
+  <div class="flex items-center space-x-16 select-none">
+    <p class="cursor-pointer">STAR</p>
+    <p class="text-black-75 cursor-pointer">home</p>
+    <p class="text-black-75 cursor-pointer">upload</p>
+    <p class="text-black-75 cursor-pointer">pricing</p>
+  </div>
+  <div class="flex items-center space-x-8 scale-[.6]">
+    <BorderedButton ariaLabel="Login">
+      <p>Login</p>
+    </BorderedButton>
+    <Button ariaLabel="Sign Up">
+      <p>Sign Up</p>
+    </Button>
+  </div>
+</navbar>
+
+<div class="mt-32 flex items-center justify-between mx-64">
+  <span>
+    <h1>find ratings <br /> & upload <br /> yours</h1>
+    <p class="text-black-75 max-w-96">
+      Enim eligendi culpa iusto consequuntur distinctio impedit ipsa quas eos
+      ipsam ipsum numquam maiores repellendus pariatur dicta aperiam, eaque
+      neque eius exercitationem?
+    </p>
+  </span>
+  <img
+    src="/undraw_reviews_ukai.svg"
+    class="h-96 mt-16 select-none"
+    alt="ratings"
+  />
+</div>
+
+<Button className ="mt-32 mx-auto text-white" ariaLabel="Get Started">
+  <p>Get Started</p>
+  <img src="/arrow_down.svg" height="24px" width="24x" class="" alt="scroll down" />
+</Button>
